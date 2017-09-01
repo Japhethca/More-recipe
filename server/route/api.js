a70cb6abbc585ba9502fe9adf503c29f864fc7ff
japhethca <chidiebere.anyigor@gmail.com>
2017-09-01 07:45:42 +0100

    feat: added more api route methods

--- server/route/api.js
+++ server/route/api.js
@@ -1,13 +1,20 @@
-const express = require('express');
-const userController = require('../controllers/userController');
-const recipeController = require('../controllers/recipeController');
+import {Router} from 'express';
+import userController from '../controllers/userController';
+// import recipeController from '../controllers/recipeController';
 
-const apiRouter = express.Router();
+const apiRouter = Router();
 
-apiRouter.post('/users/signup',  );
-apiRouter.post('/users/signin', );
-apiRouter.post('/recipes',  );
-apiRouter.put('recipes/:recipeId',  );
-apiRouter.delete('');
+apiRouter.post('/users/signup', userController.signup);
+apiRouter.post('/users/signin', userController.signin);
+// apiRouter.post('/recipes', );
+// apiRouter.put('recipes/:recipeId', recipeController.update );
+// apiRouter.delete('/recipes/:recipeId',recipeController.destroy);
+// apiRouter.get('/recipes', recipeController.retrieve);
+// apiRouter.post('/recipes/:recipeId',recipeController.create);
+// apiRouter.get('/users/:userId/recipes',userController.recipes);
+// apiRouter.get('recipes?sort=upvotes&order=ascending',recipeController.sort);
+// apiRouter.get('/user/favorites',userController.favorites);
+// apiRouter.get('/recipes/:recipeId/upvotes', recipeController.upvotes);
+// apiRouter.get('/recipes/recipeId/reviews', recipeController.reviews);
 
 module.exports = apiRouter;
