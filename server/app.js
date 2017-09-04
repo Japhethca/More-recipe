acd84851d34c5900cde6ffcb0fe85cc1a730dc82
japhethca <chidiebere.anyigor@gmail.com>
2017-09-04 20:31:51 +0100

    chores: made modifications to  the app

--- server/app.js
+++ server/app.js
@@ -1,16 +1,29 @@
-
-const express = require('express');
-const api = require('./route/api')
+// library and controller initiazation
+import express  from 'express';
+import bodyParser from 'body-parser';
+import { apiRouter, isLoggedIn } from './route/api';
+import jwt from 'jsonwebtoken';
 
 const app = express();
 
-app.use('/api', api)
+//secret for json web token
+app.set('secret_key', process.env.SECRET_KEY)
+//for parsing body content
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
+//routes
+app.use('/api', /* isLoggedIn, */ apiRouter);
 app.all('*', (req, res) => {
   res.status(404).send("404: Not Found");
 });
 
-app.listen(8000,'127.0.0.1', () => {
-  console.log("Server runnging. listening on port: 8000");
+
+// server initialization
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log("Server running. listening on port: " + port);
 });
 
+
+export {app, jwt};
