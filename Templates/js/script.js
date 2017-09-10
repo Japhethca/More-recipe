c81204ddca80b5445997a7d457fabcf9f0174f87
japhethCA <chidiebere.anyigor@gmail.com>
2017-09-10 19:09:18 +0100

    made changes to template files

--- Templates/js/script.js
+++ Templates/js/script.js
@@ -1,4 +1,18 @@
 // for navigation slide bar
   $(document).ready(function(){
     $('.button-collapse').sideNav();
-  })
+    $('.modal').modal({
+        dismissible: true, // Modal can be dismissed by clicking outside of the modal
+        opacity: .5, // Opacity of modal background
+        inDuration: 300, // Transition in duration
+        outDuration: 200, // Transition out duration
+        startingTop: '4%', // Starting top style attribute
+        endingTop: '10%', // Ending top style attribute
+        // ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
+          
+        //   console.log(modal, trigger);
+        // },
+    }
+    );
+});
+
