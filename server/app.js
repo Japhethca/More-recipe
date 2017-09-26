cf44a432354ca0d22a027e407560bfc0d817ba81
japhethca <chidiebere.anyigor@gmail.com>
2017-09-26 19:52:28 +0100

    added webpack middleware

--- server/app.js
+++ server/app.js
@@ -1,9 +1,13 @@
 // library and controller initiazation
 import express from 'express';
 import bodyParser from 'body-parser';
-import jwt from 'jsonwebtoken';
-import apiRouter from './routes/api';
 import dotenv from 'dotenv';
+import webpack from 'webpack';
+import webpackMiddleware from 'webpack-dev-middleware';
+import path from 'path';
+import webpackConfig from '../webpack.config.dev';
+import apiRouter from './routes/api';
+
 
 dotenv.config();
 
@@ -17,11 +21,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 
+app.use(webpackMiddleware(webpack(webpackConfig)));
 // more recipe api routes
 app.use('/api', apiRouter);
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Welcome to more recipe app' });
+app.get('/*', (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, '../index.html'));
 });
 app.all('*', (req, res) => {
   res.status(404).send('404: Not Found');
