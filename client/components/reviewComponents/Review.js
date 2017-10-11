dd80f3f8a8309c65100325b51092c5a7da8adefd
Chidex <andeladeveloper@Andelas-MacBook-Pro.local>
2017-10-11 07:53:58 +0100

    added support for reviews in app with addnew and review components for adding new review and viewing the reviews

---
+++ client/components/reviewComponents/Review.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import UserDetails from '../userComponent/UserDetail';
+import '../../styles/sass/reviewBox.scss';
+
+class Review extends Component {  
+  render() {
+    const {review} = this.props;
+    return (
+      <div className='my-review z-depth-3'>
+        <div>
+          <UserDetails userId={review.userId} />
+          <span className='meta-date'>{new Date(review.createdAt).toDateString()}</span>
+        </div>
+        <hr/>
+        <h5>{review.title}</h5>
+        <div>
+          {review.content}
+        </div>
+      </div>
+    )
+  }
+}
+
+Review.propTypes = {
+  reveiw: PropTypes.object.isRequired
+};
+
+export default Review;
