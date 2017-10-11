bcb6aa8d910cf32638ee526ce343d7c8a6529f22
Chidex <andeladeveloper@Andelas-MacBook-Pro.local>
2017-10-11 07:57:30 +0100

    made chages in the layout of the component

--- client/containers/Home.js
+++ client/containers/Home.js
@@ -1,22 +1,33 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
 import NavigationBar from '../components/NavigationBar';
+import Footer from '../components/Footer';
 import TopRecipes from '../components/recipeComponents/TopRecipe';
+import getAllRecipes from '../actions/requestHandlers/getAllRecipes';
+import getAllReview from '../actions/requestHandlers/getReviews';
 
 
 class Home extends Component {
+  componentDidMount(){
+    this.props.getAllRecipes();
+    this.props.getAllReview();
+  }
   render() {
     return (
-      <div>
+      <div id='main'>
         <NavigationBar />
         <TopRecipes history={this.props.history} />
+        <Footer/>
       </div>
     )
   }
 }
 
 Home.propTypes = {
-  history: PropTypes.object.isRequired
-}
+  history: PropTypes.object.isRequired,
+  getAllRecipes: PropTypes.func.isRequired,
+  getAllReview: PropTypes.func.isRequired
+};
 
-export default Home;
+export default connect(null, {getAllRecipes, getAllReview})(Home);
