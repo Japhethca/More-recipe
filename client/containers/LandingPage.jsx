1717d5cc6643bfc201ff0de05ad3c432d82a5514
japhethca <chidiebere.anyigor@gmail.com>
2017-11-15 00:06:12 +0100

    feat(landingPage): adds quotes and authentication buttons

--- client/containers/LandingPage.jsx
+++ client/containers/LandingPage.jsx
@@ -2,18 +2,19 @@ import React, { Component } from 'react';
 import '../styles/sass/landing.scss';
 
 class LandingPage extends Component {
-  componentWillMount() {
-
-  }
-
-  componentDidMount() {
-    $('.carousel.carousel-slider').carousel({ fullWidth: true });
-  }
-
   render() {
     return (
-      <div classn="carousel carousel-slider">
-        <a classn="carousel-item" href="#one!"><img src="https://lorempixel.com/800/400/food/1" /></a>
+      <div className="landing-image row">
+        <div className="quote-text col s12 m6 l6">
+            “You don’t have to cook fancy or complicated masterpieces, just good food from fresh ingredients.” <br />
+          <span>– Julia Child</span>
+        </div>
+        <div className="authenticate col s12 m6 l6">
+          <h4>Start Your Cooking Journey</h4>
+          <button className="landing-btn">Login</button> or
+          <button className="landing-btn">SignUp</button>
+          Get <span>More Recipes</span> Now By
+        </div>
       </div>
     );
   }
