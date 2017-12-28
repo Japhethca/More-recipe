edb97f8991438cd85a89dd02fb4ed26a4672fb60
japhethca <chidiebere.anyigor@gmail.com>
2017-12-28 17:21:20 +0100

    fix(navigation-UI): fixes issues with navigation UI styling

--- client/containers/LandingPage.jsx
+++ client/containers/LandingPage.jsx
@@ -1,28 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import Button from '../components/common/Button';
+import NavigationBar from '../components/navigation/NavigationBar';
+import Footer from '../components/navigation/Footer';
 import '../styles/sass/landing.scss';
 
 const LandingPage = () => (
   <div className="landing-image">
-    <div className="quote-text col s12">
-          “You don’t have to cook fancy or complicated masterpieces,
-          just good food from fresh ingredients.” <br />
-      <span>– Julia Child</span>
+    <NavigationBar bgColor="#e4e2dc94" />
+    <div className="quote-text">
+         Find and Share Best and Exciting Recipes
     </div>
-    <div className="authenticate col s12">
+    <div className="authenticate">
       <h4>Start Your Cooking Journey</h4>
       <div >
-        <Link to="/signin" href="/signin">
-          <Button text="Log In" className="landing-btn" />
+        <Link to="/signup" href="/signup" className="landing-btn">
+        Register Now
         </Link>
         Or
-        <Link to="/signup" href="/signup">
-          <Button text="Sign Up" className="landing-btn" />
+        <Link to="/signin" href="/signin" className="landing-btn">
+        Log In
         </Link>
       </div>
     </div>
+    <Footer />
   </div>
 );
 
